d15cb8fc66f7057065518f5fb2fdeba49f85b6d8
muhammadali2564 <ali.bashir1947@gmail.com>
2024-02-29 20:08:26 +0500

    lab01 ex 2 completed

--- lab01/exercise2/pwd_checker.c
+++ lab01/exercise2/pwd_checker.c
@@ -22,13 +22,13 @@ For the simplicity of this exercise:
 /* Returns true if the length of PASSWORD is at least 10, false otherwise */
 bool check_length(const char *password) {
     int length = strlen(password);
-    bool meets_len_req = (length <= 10);
+    bool meets_len_req = (length >= 10);
     return meets_len_req;
 }
 
 /* Returns true if LETTER is in the range [LOWER, UPPER], false otherwise */
 bool check_range(char letter, char lower, char upper) {
-    bool is_in_range = (letter > lower && letter < upper);
+    bool is_in_range = (letter >= lower && letter <= upper);
     return is_in_range;
 }
 
@@ -59,7 +59,7 @@ bool check_lower(const char *password) {
 /* Returns true if PASSWORD contains at least one number, false otherwise */
 bool check_number(const char *password) {
     while (*password != '\0') {
-        if (check_range(*password, 0, 9)) {
+        if (check_range(*password, '0', '9')) {
             return true;
         }
         ++password;
@@ -75,22 +75,22 @@ bool check_name(const char *first_name, const char *last_name, const char *passw
         will evaluate to True */
     const char *first = strstr(password, first_name);
     const char *last = strstr(password, last_name);
-    return (first && last);
+    return (!(first || last));
 }
 
 /* Returns true if PASSWORD meets the conditions specified above */
 bool check_password(const char *first_name, const char *last_name, const char *password) {
     bool length, upper, lower, number, name;
     lower = check_lower(password);
-    assert(lower);
+    //	assert(lower);
     length = check_length(password);
-    assert(length);
+    //	assert(length);
     name = check_name(first_name, last_name, password);
-    assert(name);
+    //	assert(name);
     number = check_number(password);
-    assert(number);
+    //	assert(number);
     upper = check_upper(password);
-    assert(upper);
+    //	 assert(upper);
     return (lower && length && name && upper && number);
     
 }
